5fc78707ec4cf4670440b1cc34132a5697b6d224
laurentnunez <lnunez@hotmail.fr>
2022-07-19 11:08:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.scss';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { InputGroup, Input, Button, CardGroup, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+
+import { InputGroup, Input, Button } from 'reactstrap';
+
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+
 
 //https://gateway.marvel.com:443/v1/public/characters?apikey=1ffdef23335e8a0d66472e6377bfefdd
 
@@ -29,7 +33,7 @@ function App() {
 const [characters, setCharacters] = useState([]);
 
 useEffect(()=> {
-  axios.get('https://gateway.marvel.com:443/v1/public/characters?limit=50&ts=1&apikey=1ffdef23335e8a0d66472e6377bfefdd&hash=d23c60ea2b610ea755740abc2b502cb1')
+  axios.get('https://gateway.marvel.com:443/v1/public/characters?name=wolverine&ts=1&apikey=1ffdef23335e8a0d66472e6377bfefdd&hash=d23c60ea2b610ea755740abc2b502cb1')
 .then(response=>{
   setCharacters(response.data.data.results)
   
@@ -42,79 +46,40 @@ console.log(characters);
 
   return (
     <div className="App">
-      
+      <nav className="navbar bg-light" id="navbar">
+        <div className="container-fluid">
+          <img className="logo__marvelous" alt="logo-marvelous" src="../../logo-marvelous.png"/>
+          <form className='d-flex input-group w-auto'>
+            <input type='search' className='form-control' placeholder='Entrer votre personnage' aria-label='Search' />
+            <MDBBtn color='danger'>Rechercher</MDBBtn>
+          </form>
+        </div>
+      </nav>
 
-      <nav className="navbar bg-light">
-  <div className="container-fluid">
-  <h1>MARVELOUS</h1>
-  <InputGroup>
-    <Input />
-    <Button>
-      Rechercher
-    </Button>
-  </InputGroup>
+    
+    <div className='card-list'>
+    {characters.map(character=>(
+    <MDBCard style={{ maxWidth: '20rem' }}>
+      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+        <MDBCardImage src={`${character.thumbnail.path}.${character.thumbnail.extension}`} fluid alt='...' />
+        <a>
+          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+        </a>
+      </MDBRipple>
+      <MDBCardBody>
+        <MDBCardTitle>{character.name}{character.title}</MDBCardTitle>
+        <MDBCardText>
+        {character.description}
+        </MDBCardText>
+        <MDBBtn color='danger' href='#'>Button</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+    ))}
   </div>
-</nav>
-
-
-
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-      
-        {characters.map(character=>(
-
-        
-      <div className="col"key={character.id}>
-      <div className="card" style={{width: "18rem", height:"18rem"}}>
-      <p className="card-text">{character.name}</p>
-        <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} className="card-img-top" style={{width: "100%", height:"100%"}} alt={character.name}/>
-          <div className="card-body">
-          
-          <p className="card-text">{character.description}</p>
-          </div>
-      </div>
-      </div>
-      ))}
-    </div>
-    
-    
-
-
-<CardGroup>
-  {characters.map(character=>(
-  <Card>
-    <CardImg
-      alt="Card image cap"
-      src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-      {character.name}
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-      {character.description}
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-  ))}
-</CardGroup>
-
-
-
-
 </div>
 
   );
+
 }
 
 export default App;
